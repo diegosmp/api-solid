259498e762607a745ae1afa62132bb714a20ea2e
diegosmp <diiegosmp@gmail.com>
2025-01-25 00:45:06 -0300

    Add Feature: Route User Register

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import fastify from 'fastify'
-import { register } from './http/controllers/register'
+import { appRoutes } from './http/routes'
 
 const app = fastify()
 
-app.post('/users', register)
+app.register(appRoutes)
 
 export default app
