c6738b8c46d0ca59b08aab54145c172e5648e1cb
diegosmp <diiegosmp@gmail.com>
2025-02-10 22:11:50 -0300

    Add feature: Test check in twice same day

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckinUseCase } from './check-in'
 
@@ -9,14 +9,38 @@ describe('Check-In Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckinUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('Should be able to check in', async () => {
+    vi.setSystemTime(new Date(2025, 0, 20, 8, 0, 0))
+
     const { checkIn } = await sut.execute({
       gymId: 'gymId-01',
       userId: 'userId-01',
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('Should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2025, 1, 10, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gymId-01',
+      userId: 'userId-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gymId-01',
+        userId: 'userId-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
