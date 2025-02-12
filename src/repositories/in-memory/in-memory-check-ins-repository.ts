2914adc9d4183ff5b40aa33e5efc54725f90c13d
diegosmp <diiegosmp@gmail.com>
2025-02-11 23:59:28 -0300

    Add Feature: Use Case, In Memory, Test, Validate Check Ins

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,25 @@ import dayjs from 'dayjs'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn
+    }
+
+    return checkIn
+  }
+
+  async finById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id)
+
+    if (!checkIn) {
+      return null
+    }
+    return checkIn
+  }
+
   async countByUserId(userId: string) {
     return this.items.filter((item) => item.user_id === userId).length
   }
---
+++ src/use-cases/validate-check-ins.spec.ts
@@ -0,0 +1,42 @@
+import { expect, describe, it, beforeEach, afterEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { ValidateCheckInsUseCase } from './validate-check-ins'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInsUseCase
+
+describe('Validate Check-In Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new ValidateCheckInsUseCase(checkInsRepository)
+
+    // vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    // vi.useRealTimers()
+  })
+
+  it('Should be able to validate the check-in', async () => {
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('Should not be able to validate an inexistent check-in', async () => {
+    await expect(() =>
+      sut.execute({
+        checkInId: 'inexistent-check-in-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
